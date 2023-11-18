00045c68a9318526e30499f15bf0c36d04ab0ec2
OrigamingWasTaken <mabarleycorne@gmail.com>
2023-11-18 13:48:11 +0100

    Fixed start script .env check

--- scripts/start.js
+++ scripts/start.js
@@ -2,7 +2,7 @@ const { existsSync, copyFileSync } = require("fs")
 const { join, dirname } = require("path")
 
 try {
-    if (!existsSync(join(dirname(__dirname)), ".env")) {
+    if (!existsSync(join(dirname(__dirname), ".env"))) {
         copyFileSync(join(__dirname, "assets/.env.template"), join(dirname(__dirname),".env"))
         console.log("\x1b[0;31mYou did not have a .env in your project root. It has automatically been created. Please fill the mising variables.\x1b[0m")
         process.exit(1)
