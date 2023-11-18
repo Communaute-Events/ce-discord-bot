3d56c44a7e6371eed983b45e2ef7b0165e385c24
OrigamingWasTaken <mabarleycorne@gmail.com>
2023-11-18 13:42:58 +0100

    Changed exit script

--- scripts/start.js
+++ scripts/start.js
@@ -4,7 +4,8 @@ const { join, dirname } = require("path")
 try {
     if (!existsSync(join(dirname(__dirname)), ".env")) {
         copyFileSync(join(__dirname, "assets/.env.template"), join(dirname(__dirname),".env"))
-        throw new Error("You did not have a .env in your project root. It has automatically been created. Please fill the mising variables.")
+        console.log("\x1b[0;31mYou did not have a .env in your project root. It has automatically been created. Please fill the mising variables.\x1b[0m")
+        process.exit(1)
     }
 } catch (error) {
     console.log(error)
