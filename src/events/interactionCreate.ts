cb7e90233be78fe4c802c4ea04e1826740764ba7
OrigamingWasTaken <mabarleycorne@gmail.com>
2023-11-19 19:47:48 +0100

    Fixed duplicate commands

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,10 +1,21 @@
 import { logging } from "@src/core/utilities";
-import { CommandInteraction } from "discord.js";
+import { CommandInteraction, REST, Routes } from "discord.js";
 
 export const event =  {
     name: "interactionCreate",
     async execute(interaction: CommandInteraction) {
         if (!interaction.isChatInputCommand()) return;
+        // Resets commands for the guild if it has leftover slash commands
+        if ("guilds" in interaction.client.commands.get(interaction.commandName) && !interaction.client.commands.find(cmd => {
+            return cmd.guilds && cmd.guilds.includes(interaction.guild.id)
+        })) {
+            const rest = new REST().setToken(process.env.token);
+            const data = await rest.put(Routes.applicationGuildCommands(process.env.clientId, interaction.guildId), {
+                body: []
+            })
+            interaction.reply({content: "The slash command you just used doesn't exist anymore.", ephemeral: true})
+            return
+        }
 
         const command = interaction.client.commands.get(interaction.commandName)
 
@@ -15,7 +26,7 @@ export const event =  {
 
         try {
             await command.execute(interaction)
-            logging(`${interaction.user.globalName} executed the command "${interaction.commandName}" in guild "${interaction.guildId} at ${new Date().toLocaleString()}`,"minimal",false)
+            logging(`${interaction.user.globalName} executed the command "${interaction.commandName}" in guild "${interaction.guildId}" at "${new Date().toLocaleString()}"`,"minimal",false)
         } catch (error) {
             logging(`There was an error executing "${interaction.commandName}":\n${error}`,"error")
             if (interaction.replied || interaction.deferred) {
