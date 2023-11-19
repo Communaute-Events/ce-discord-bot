9c155ddd199dd60d427708e2f4f31e86572ce958
OrigamingWasTaken <mabarleycorne@gmail.com>
2023-11-19 20:11:54 +0100

    Fixed removed command handling

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -5,8 +5,14 @@ export const event =  {
     name: "interactionCreate",
     async execute(interaction: CommandInteraction) {
         if (!interaction.isChatInputCommand()) return;
+        const command = interaction.client.commands.get(interaction.commandName)
+        if (!command) {
+            logging(`No Command found with name "${interaction.commandName}"`, "error");
+            return
+        }
+
         // Resets commands for the guild if it has leftover slash commands
-        if ("guilds" in interaction.client.commands.get(interaction.commandName) && !interaction.client.commands.find(cmd => {
+        if ("guilds" in command && !interaction.client.commands.find(cmd => {
             return cmd.guilds && cmd.guilds.includes(interaction.guild.id)
         })) {
             const rest = new REST().setToken(process.env.token);
@@ -14,13 +20,6 @@ export const event =  {
                 body: []
             })
             interaction.reply({content: "The slash command you just used doesn't exist anymore.", ephemeral: true})
-            return
-        }
-
-        const command = interaction.client.commands.get(interaction.commandName)
-
-        if (!command) {
-            logging(`No Command found with name "${interaction.commandName}"`, "error");
             return
         }
 
