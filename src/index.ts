ea86600043446cb4953f9b398d0a62ca3e4fc325
OrigamingWasTaken <mabarleycorne@gmail.com>
2023-11-29 16:49:35 +0100

    Added WebSocket reconnect

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,60 @@ const client = new Client({intents: [GatewayIntentBits.Guilds]});
     client.commands = await getCommands()
 })()
 
+function checkWs(ws: WebSocket) {
+    if (ws.readyState != WebSocket.OPEN) {
+        logging(`WebSocket connection has been established. The bot is operational.`,"success")
+    } else {
+        setTimeout(checkWs,500)
+    }
+}
+
+let isReconnecting;
+
+async function wsConnect() {
+    function wsError(error) {
+        if (isReconnecting) return
+        isReconnecting = true
+        logging(`WebSocket error, retrying to connect in 10 seconds.\n${error}: ${error.message}`, "error");
+        // Retry after 10 seconds
+        setTimeout(wsConnect, 10000);
+    }
+
+    isReconnecting = false
+    try {
+        const response = await fetch("https://raw.githubusercontent.com/Communaute-Events/paths/main/paths.json", {
+            method: "GET",
+            headers: { "Content-Type": "application/json" }
+        });
+
+        const res = await response.json();
+        const ws = new WebSocket(res.websocket)
+
+        checkWs(ws)
+
+        ws.on("message", msg => {
+            try {
+                const data = JSON.parse(msg.toString());
+                if ("_type" in data && data._type === "event") {
+                    alert(data, client);
+                }
+            } catch (error) {
+                logging(`Error in ws:\n${error}`,"error");
+            }
+        });
+
+        ws.on("close", (code, reason) => {
+            wsError({message: reason})
+        });
+
+        ws.on("error", error => {
+            wsError(error)
+        });
+    } catch (error) {
+        wsError({message: error})
+    }
+}
+
 client.once(Events.ClientReady, async(c) => {
     logging("Init. Client...","info")
 	logging(ansi(`Logged in as %bold%${c.user.tag}%end%%light_green%!%end%`),"success");
@@ -28,23 +82,7 @@ client.once(Events.ClientReady, async(c) => {
     await deployCommands()
     logging(`All slash commands have been deployed.`,"success")
     logging(`Connecting to the Event Websocket`,"info")
-    await fetch("https://raw.githubusercontent.com/Communaute-Events/paths/main/paths.json",{
-        method: "GET",
-        headers: { "Content-Type": "application/json" }
-    }).then(res => res.json().then(res => {
-        const ws = new WebSocket(res.websocket)
-        ws.on("message",msg => {
-            try {
-                const data = JSON.parse(msg.toString())
-                if ("_type" in data && data._type == "event") {
-                    alert(data, client)
-                }
-            } catch (error) {
-                console.log(`Error in ws:\n${error}`)
-            }
-        })
-    }))
-    logging(`Connection to the websocket has been established. The bot is ready to run!`,"success")
+    wsConnect()
 });
 
 // Login, needs to be at the bottom
