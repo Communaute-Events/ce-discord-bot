1d8881409506c7504de7862a0fc08783deb2f3c4
OrigamingWasTaken <mabarleycorne@gmail.com>
2023-12-14 19:55:08 +0100

    Experimental embed command

---
+++ src/commands/private/embed/embed.ts
@@ -0,0 +1,31 @@
+// @command
+
+import { HelpCommandConfig } from '@src/types';
+import { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder, ChatInputCommandInteraction } from 'discord.js'
+const Theme = loadYaml("bot/info.yml")
+const HelpConfig: HelpCommandConfig = loadYaml("commands/help.yml")
+import { DiscoHook } from '@src/types';
+
+// @command
+// here cuz otherwise tsc removes it
+export default {
+	data: new SlashCommandBuilder()
+		.setName('embed')
+		.setDescription('Sends the embed from the input')
+        .addStringOption(option =>
+            option
+                .setRequired(true)
+                .setName("embed")
+                .setDescription("Embed data")),
+	async execute(interaction: ChatInputCommandInteraction) {
+        try {
+		    const data: DiscoHook = JSON.parse(interaction.options.getString("embed"))
+            await interaction.reply({content: data.content, embeds: data.embeds});
+        } catch (error) {
+            await interaction.reply("```" + error + "```")
+        }
+	},
+    guilds: [
+        "1171557764703723611"
+    ]
+};
